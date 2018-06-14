dbdd7dfe7bd7a4493e2b1118d905946012c6cf88
Shatoidil <shatoidil@gmail.com>
2018-06-14 23:22:57 +0300

    Код ревью

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 
 $(document).ready(function() {
-    $.getJSON("https://rawgit.com/Shatoidil/kontur/master/kladr.json")
-    .done(function(data) {
-    const cities = data;
-    $('#cityInput').on('input', function() {
 
-        let userInput = $(this).val();
-        let search = '';
-    
+    let cities, search, userInput;
+
+    $.getJSON("https://rawgit.com/Shatoidil/kontur/master/kladr.json")
+                   .done(function(data) {
+                    cities = data;
+                    });
+
+    $('#cityInput').on('input', function() {
+        
+        userInput = $(this).val();
+
         if(userInput.length > 2) {
-                search = cities.filter(
+
+            search = cities.filter(
                 function(value){
                     return value.City.toUpperCase().includes(userInput.toUpperCase());
                 }
             )
             if (search === undefined || search.length == 0) {
+
+                    showOrHide(true);
                     $('.result').remove();
-                    $('#citiesResult').removeClass('results transition hidden')
-                    .addClass('results transition visible')
-                    .css('display: block !important');
-    
                     $('#citiesResult').append( '<a class="result">' 
                     + '<div class="content"><div class="title">' 
                     + 'Ничего не найдено' + '</div></div></a>' );
                 } 
             else {
+
                     $('.result').remove();
-                    $('#citiesResult').removeClass('results transition hidden')
-                    .addClass('results transition visible')
-                    .css('display: block !important');
-    
+                    showOrHide(true);
                     search.forEach(element => {
                         $('#citiesResult').append( '<a class="result">' 
                         + '<div class="content"><div class="title">' 
@@ -38,9 +39,8 @@ $(document).ready(function() {
                 }  
             }
         else {
-            $('#citiesResult').removeClass('results transition visible')
-            .addClass('results transition hidden')
-            .css('display: block !important');
+
+            showOrHide(false);
         }
     
     });
@@ -48,13 +48,22 @@ $(document).ready(function() {
     
     $('#citiesResult').on('click', '.result', function(){
         $('#cityInput').val($(this).text());
-        $('#citiesResult').removeClass('results transition visible')
-        .addClass('results transition hidden')
-        .css('display: block !important');
+        showOrHide(false);
     });
 
+    function showOrHide(switchState){
+        if(switchState) {
 
+            return $('#citiesResult').removeClass('results transition hidden')
+           .addClass('results transition visible')
+           .css('display: block !important');
+        }
+        else{
 
+            return $('#citiesResult').removeClass('results transition visible')
+            .addClass('results transition hidden')
+            .css('display: block !important');
+        }
+    }
 
-    });
 });
