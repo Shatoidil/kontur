e61834617428ba9b5453e0a062bc744fc3afb752
Shatoidil <shatoidil@gmail.com>
2018-06-18 10:17:55 +0300

    Очистка пробелов

--- main2.js
+++ main2.js
@@ -4,49 +4,38 @@
             let settings = {
                 url :'',
             };
-
             options = $.extend(settings, options);
-            
+
             let loadedData  = [];
             let searchArray = [];
             let input = this;
             let results = $(".results");
             let showAnimation = true;
-      
-
-
 
             $.getJSON(options.url).done(
                 function(data) { 
                     loadedData = data;
                 }
             );
-
             results.on("click", ".result", function() {
                 input.val($(this).text())
                 .attr('city_id', $(this).closest('a').attr('city_id'));
                 showOrHideResultsDiv(showDivs = false);
                 showAnimation = true;
             });
-            input.on('keyup', function(e){
-                               
+            input.on('keyup', function(e){                              
                 if (input.val().length >= 2){                
                     searchArray = loadedData.filter(function(value) {
                         return value.City.toUpperCase()
                         .includes(input.val().toUpperCase());
-                    });
-                    
+                    });                   
                     if(searchArray.length){
                         if(showAnimation){
                             showOrHideResultsDiv(showDivs = true);
                             showAnimation = false;
-                        }
-                        
+                        }                       
                         // очистка поиска
                         $(".result").remove();
-
-
-
                     }
                     else{
                         if(showAnimation){
@@ -59,14 +48,11 @@
                         "Ничего не найдено" +
                         "</div></div></a>");
                     }
-
-
                 }
                 else {                   
                     showOrHideResultsDiv(showDivs = false);
                     showAnimation = true;
                 }
-
             });
             function showOrHideResultsDiv(showDivs) {
                 if(showDivs){
@@ -74,13 +60,8 @@
                 }
                 else{
                     results.attr('class', 'results').hide();
-                }
-            
-            };
-            
+                }          
+            };            
         }
-
-
-
     });
 })(jQuery);
