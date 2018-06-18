454637e3de20f67211a55ab822da7be17c6b8bf6
Shatoidil <shatoidil@gmail.com>
2018-06-18 13:17:57 +0300

    Теперь работает с IE добавил полифил

--- js/main2.js
+++ js/main2.js
@@ -1,4 +1,5 @@
 (function($) {
+    'use strict';
     $.fn.extend({
         autoSearch: function(options) {
             let settings = {
@@ -11,6 +12,7 @@
             let input = this;
             let results = $(".results");
             let showAnimation = true;
+            let showDivs = true;
 
             $.getJSON(options.url).done(
                 function(data) { 
@@ -20,26 +22,35 @@
             results.on("click", ".result", function() {
                 input.val($(this).text())
                 .attr('city_id', $(this).closest('a').attr('city_id'));
-                showOrHideResultsDiv(showDivs = false);
+                showResult(showDivs = false);
                 showAnimation = true;
             });
-            input.on('keyup', function(e){                              
-                if (input.val().length >= 2){                
+            input.on('keyup', function(e){ 
+                if (input.val().length >= 2){
+
                     searchArray = loadedData.filter(function(value) {
                         return value.City.toUpperCase()
                         .includes(input.val().toUpperCase());
-                    });                   
+                    });
                     if(searchArray.length){
                         if(showAnimation){
-                            showOrHideResultsDiv(showDivs = true);
+                            showResult(true);
                             showAnimation = false;
-                        }                       
+                        }
                         // очистка поиска
                         $(".result").remove();
+                        for (let i = 0; i < searchArray.length; i++) {
+                            results.append(
+                              '<a city_id="' + searchArray[i].Id + '" class="result">' +
+                                '<div class="content"><div class="title">' +
+                                searchArray[i].City +
+                                "</div></div></a>"
+                            );
+                        }
                     }
                     else{
                         if(showAnimation){
-                            showOrHideResultsDiv(showDivs = true);
+                            showResult(true);
                             showAnimation = false;
                         }
                         $(".result").remove();
@@ -49,19 +60,20 @@
                         "</div></div></a>");
                     }
                 }
-                else {                   
-                    showOrHideResultsDiv(showDivs = false);
+                else {
+                    showResult(false);
                     showAnimation = true;
                 }
             });
-            function showOrHideResultsDiv(showDivs) {
-                if(showDivs){
+            function showResult(value) {
+                if(value){
                     results.attr('class', 'results').transition('scale');
                 }
                 else{
-                    results.attr('class', 'results').hide();
-                }          
-            };            
+                    //
+                    results.attr('class', 'results').css({"visibility":"hidden"});
+                }  
+            };
         }
     });
 })(jQuery);
---
+++ js/polyfill.js
@@ -0,0 +1,7 @@
+// Поддержка includes
+if (!String.prototype.includes) {
+    String.prototype.includes = function() {
+      'use strict';
+      return String.prototype.indexOf.apply(this, arguments) !== -1;
+    };
+  }
