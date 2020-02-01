c670da67e4da6abb287a6a84b13fdd2b2996d6e4
Shato <shatoidil@gmail.com>
2020-02-01 17:24:25 +0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,8 +1,8 @@
-<script type="text/javascript">
+
 $(document).ready(function() {
 
 console.log('dsdsd')
 
 $( ".js-store-prod-name.js-product-name.t-store__card__title" ).wrapInner("<div class='bbbb'></div>");
 });
-</script>
+
